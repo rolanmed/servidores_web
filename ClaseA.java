76e11419fa33fcbdd95a17ce031ee06d96db852b
rolanmed <rolanmed@gmail.com>
2019-11-13 18:41:58 -0600

    subida de archivo

---
+++ ClaseA.java
@@ -0,0 +1,15 @@
+public class ClaseA{
+
+  private int x;
+  private int y;
+  
+  public ClaseA(){
+   x = 0;
+    y = 0;
+  }
+  
+  public void mensaje(){
+	  System.out.println("Hola");
+  }
+  
+ }
